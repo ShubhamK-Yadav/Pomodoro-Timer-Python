04a8d9ee0177caf85dafd1b063ff70c4b5724aa3
ShubhamK-Yadav <syadav2098@gmail.com>
2025-07-17 23:31:27 +0100

    Timer logic and display complete

--- display_timer.py
+++ display_timer.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 import customtkinter as ctk
-class TimerDisplay(ctk.CTkLabel):
-    def __init__(self):
 
-    def
+class TimerDisplay(ctk.CTkLabel):
+    def __init__(self, parent, timer_logic):
+        self.timer = timer_logic
+        super().__init__(parent, text=self.timer.formatted_time(), font=("Comic Sans MS", 36))
+        self.update_timer()
+
+    def update_timer(self):
+        self.configure(text= self.timer.formatted_time())
+        # Do not pass () with update_timer, only reference passed
+        # Telling it what to call essentially instead of calling it right now
+        self.after(1000, self.update_timer)
--- main_application.py
+++ main_application.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 import customtkinter as ctk
+from Timer import Timer as timer_logic
+from display_timer import TimerDisplay as timer_display
 
 class MainApplication(ctk.CTkFrame):
     def __init__(self, parent) -> None:
         super().__init__(parent)
+        self.timer_logic = timer_logic(minutes=25)
+        self.timer_display = timer_display(self, self.timer_logic)
+        self.timer_display.pack(pady=10)
 
 
 if __name__ == "__main__":
@@ -15,4 +20,5 @@ if __name__ == "__main__":
     root.geometry("600x500")
     root.title("Pomodoro-App")
     app = MainApplication(root)
+    app.pack(expand=True, fill="both")
     root.mainloop()
